c087a548237498c97ca7044fc7c8b120a8e71774
tgrall <tugdual@gmail.com>
2022-09-12 18:08:59 +0200

    add inputs

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const github = require('@actions/github');
 
         const githubUser = core.getInput('github_user');
         const githubToken = core.getInput('github_token');
+        const githubOrg = core.getInput('github_organization'); 
+        const octokit = new github.getOctokit(githubToken);
 
-
-        core.notice(`My Action is called by ${githubUser}  `);
+        core.notice(`My Action is called by ${githubUser} for ${{githubOrg}} `);
 
 
 
